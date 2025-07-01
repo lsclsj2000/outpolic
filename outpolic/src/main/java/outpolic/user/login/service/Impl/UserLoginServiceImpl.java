7862c2006e18578c3b42ebfb90e8e72ce941f8f2
sepal <sepal@192.168.219.115>
2025-07-02 00:08:51 +0900

    [outpolic-ssy] 로그인 에러 해결을 위한 log 메세지 삽입 상세 설명 : 로그인시 input은 받으나 db에서 회원을 찾아오지 못함을 확인.

--- outpolic/src/main/java/outpolic/user/login/service/Impl/UserLoginServiceImpl.java
+++ outpolic/src/main/java/outpolic/user/login/service/Impl/UserLoginServiceImpl.java
@@ -4,12 +4,14 @@ package outpolic.user.login.service.Impl;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import outpolic.common.domain.Member;
 import outpolic.user.login.mapper.UserLoginMapper;
 import outpolic.user.login.service.UserLoginService;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserLoginServiceImpl implements UserLoginService {
 	private final UserLoginMapper userLoginMapper;
 	
@@ -17,7 +19,8 @@ public class UserLoginServiceImpl implements UserLoginService {
 	@Override
 	public Member loginUser(String memberId, String memberPw) {
 		Member member =userLoginMapper.findMemberById(memberId);
-		
+		log.info("memberId: {}, pw: {}", memberId, memberPw);
+		log.info("DB에서 찾은 회원: {}", member);
 		if(member != null && member.getMemberPw().equals(memberPw)) {
 			return member;
 		}
