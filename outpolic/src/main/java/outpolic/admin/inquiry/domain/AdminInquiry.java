b90dc2b3079c3766aafba80c299d91ccca323433
tkfk1130 <salang291@gmail.com>
2025-07-04 19:22:22 +0900

    [outpolic-YSR] admin inquiry 패키지 생성 detail : 관리자 문의 페이지 뷰, 컨트롤러, DTO 추가

---
+++ outpolic/src/main/java/outpolic/admin/inquiry/domain/AdminInquiry.java
@@ -0,0 +1,33 @@
+package outpolic.admin.inquiry.domain;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class AdminInquiry {
+	private String inquiryCode;
+	private String memberName;
+	private String inquiryTypeName;
+	private String inquiryTitle;
+	private String inquiryCn;
+	private int inquirySecret;
+	private String inquiryRegDate;
+	private String inquiryProcessCn;
+	private String adminCode;
+	private String inquiryTypeCode;
+	private String memberCode;
+	
+	private String inquiryTypeExpln; 
+	private String statusCode;
+	private String inquiryTypeRegAdm; 
+	private String inquiryTypeRegDate;
+	private String inquiryTypeMdfcnAdm; 
+	private String inquiryTypeMdfcnDate;
+	
+	private String inquiryProcessAnsCn;
+    private String inquiryProcessRegDate;
+    private String adminName;
+    
+    private List<AdminInquiryFile> userInquiryFiles;
+}
