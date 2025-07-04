040f11e32f500c686bbc95c4c640ccc06a109ac9
shindidit <bionell430@gmail.com>
2025-07-04 17:19:18 +0900

    [outpolic-SSY] 회원가입 기능 구현중 detail : 마이페이지 개인정보수정이랑 js파일 공유해서 썼더니 데이터 미리 매핑되서 들어가서 분하지만 파일 분리함. 유지보수에는 더 좋겠지. 중복확인 코드 짜는중에 어플리케이션 프로퍼티스 수정한것때문에 서버 안켜져서 일단 한번 커밋함

--- outpolic/src/main/java/outpolic/user/register/controller/UserRegisterController.java
+++ outpolic/src/main/java/outpolic/user/register/controller/UserRegisterController.java
@@ -1,19 +1,23 @@
 package outpolic.user.register.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.RequiredArgsConstructor;
 import outpolic.common.domain.Member;
 import outpolic.user.register.service.UserRegisterService;
 
 @Controller
+@RequiredArgsConstructor
 public class UserRegisterController {
 	
-	private UserRegisterService userRegisterService;
+	private final UserRegisterService userRegisterService;
     // 회원가입 페이지 시작
     // 기업회원, 일반회원 선택 페이지
     @GetMapping("/choiceRegister")
@@ -33,6 +37,14 @@ public class UserRegisterController {
         member.setMemberCode(newMemberCode);
         userRegisterService.registerMember(member);
         return "redirect:/login"; // 또는 가입 완료 페이지
+    }
+    
+    @GetMapping("/user/checkDuplicate")
+    @ResponseBody
+    public Map<String, Boolean> checkDuplicate(@RequestParam String type,
+                                               @RequestParam String value) {
+        boolean isUserInfoDuple = userRegisterService.isUserInfoDuple(type, value);
+        return Map.of("duplicate", isUserInfoDuple);
     }
     
 }
