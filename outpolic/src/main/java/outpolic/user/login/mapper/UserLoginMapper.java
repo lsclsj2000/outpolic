5d41bcdbc69f07f0b11d01430b592bc0d8df4d50
sepal <sepal@DESKTOP-HURAQ4I>
2025-07-07 19:42:22 +0900

    충돌나서 수정하고 커밋한번합니다

--- outpolic/src/main/java/outpolic/user/login/mapper/UserLoginMapper.java
+++ outpolic/src/main/java/outpolic/user/login/mapper/UserLoginMapper.java
@@ -7,5 +7,8 @@ import outpolic.common.domain.Member;
 
 @Mapper
 public interface UserLoginMapper {
+	//로그인 회원 찾아오기
 	Member findMemberById(@Param("memberId") String memberId);
+	//로그인시 기록테이블에 데이터 insert
+	void insertLoginHistory(@Param("memberId") String memberId);
 }
