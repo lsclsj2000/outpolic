04b7ac070f79f4268629abc05c83b5d194ec94f9
uzuMakiOneStar <wlfkfss2323@gmail.com>
2025-07-08 23:19:32 +0900

    [outpolic-KHB] 메인페이지 및 유저레이아웃 메인 수정 도중 백업 detail : 아직 다 못함 ㅠ

--- outpolic/src/main/resources/static/user/assets/js/userRanking.js
+++ outpolic/src/main/resources/static/user/assets/js/userRanking.js
@@ -1,46 +1,26 @@
+// userRanking.js
 $(document).ready(function(){
-		    // HTML에서 만든 슬라이더 컨테이너의 클래스명('.popular-outsourcing-slider')을 선택합니다.
-		    $('.popular-outsourcing-slider').slick({
-		        dots: false,              // 아래쪽 점 네비게이션 비활성화
-		        infinite: false,          // 무한 반복 비활성화 (끝까지 가면 멈춤)
-		        speed: 500,               // 슬라이드 전환 속도 (0.5초)
-		        slidesToShow: 5,          // ★ 한 번에 보여줄 아이템 개수
-		        slidesToScroll: 5,        // ★ 한 번에 넘길 아이템 개수
-		        
-		        // 화살표 버튼 커스터마이징 (템플릿의 아이콘 클래스 사용)
-		        prevArrow: '<button type="button" class="slick-prev"><i class="fi-rs-arrow-small-left"></i></button>',
-		        nextArrow: '<button type="button" class="slick-next"><i class="fi-rs-arrow-small-right"></i></button>',
-		        
-		        // 반응형 웹 설정 (화면 크기에 따라 보여줄 아이템 개수 조절)
-		        responsive: [
-		            {
-		                breakpoint: 1300, 
-		                settings: {
-		                    slidesToShow: 4,
-		                    slidesToScroll: 4
-		                }
-		            },
-		            {
-		                breakpoint: 992,  
-		                settings: {
-		                    slidesToShow: 3,
-		                    slidesToScroll: 3
-		                }
-		            },
-		            {
-		                breakpoint: 768,  
-		                settings: {
-		                    slidesToShow: 2,
-		                    slidesToScroll: 2
-		                }
-		            },
-		            {
-		                breakpoint: 576,  
-		                settings: {
-		                    slidesToShow: 1,
-		                    slidesToScroll: 1
-		                }
-		            }
-		        ]
-		    });
-		});
+    var $slider = $('.popular-outsourcing-slider');
+
+    if ($slider.length > 0 && $slider.find('.slider-item').length > 0) {
+        
+        $slider.slick({
+            // ... (slidesToShow, slidesToScroll 등 다른 옵션은 그대로) ...
+            
+            // ★★★ 화살표 옵션을 아래와 같이 변경합니다 ★★★
+            arrows: true, // 화살표를 표시
+            
+            // prevArrow, nextArrow 옵션: 생성될 화살표의 HTML 코드를 직접 정의합니다.
+            // 버튼에 'slick-btn' 이라는 커스텀 클래스를 추가해서 CSS로 제어하기 쉽게 만듭니다.
+            prevArrow: '<button type="button" class="slick-prev slick-btn"><i class="fi-rs-arrow-small-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next slick-btn"><i class="fi-rs-arrow-small-right"></i></button>',
+
+            infinite: false,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            draggable: false,
+            swipe: false,
+            // (responsive 옵션은 그대로 둡니다)
+        });
+    }
+});
