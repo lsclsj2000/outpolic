b729dc95c0b2288a4a61ab4dab5ff8074d6297d0
tkfk1130 <salang291@gmail.com>
2025-07-09 13:41:28 +0900

    [outpolic-YSR] 회의 전 중간 커밋 detail : 신고 타입 등록 페이지 만들다가 말았음

--- outpolic/src/main/java/outpolic/admin/declaration/service/AdminDeclarationService.java
+++ outpolic/src/main/java/outpolic/admin/declaration/service/AdminDeclarationService.java
@@ -5,6 +5,10 @@ import java.util.List;
 import outpolic.admin.declaration.domain.AdminDeclaration;
 
 public interface AdminDeclarationService {
+	
+	// 신고 타입 자원 조회
+	List<AdminDeclaration> getAdminDeclarationTypeList();
+	
 	// 신고 내역 목록 조회
-		List<AdminDeclaration> getAdminDeclarationList();
+	List<AdminDeclaration> getAdminDeclarationList();
 }
