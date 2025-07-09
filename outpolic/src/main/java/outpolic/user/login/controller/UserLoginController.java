ca8e2ea75361d27263f8045c0bb2d369fb785750
shindidit <bionell430@gmail.com>
2025-07-09 12:18:30 +0900

    [outpolic-SSY] 로그인 분기처리 완료 detail : 기업회원 회원가입 만들기 위해 그 전에 한번 커밋함. 로그인 기능 테스트해보고싶은사람은 db에 평문 비밀번호 되어있는 것 가져와서 테스트해보십쇼

--- outpolic/src/main/java/outpolic/user/login/controller/UserLoginController.java
+++ outpolic/src/main/java/outpolic/user/login/controller/UserLoginController.java
@@ -48,7 +48,7 @@ public class UserLoginController {
         
         if (isMatched) {
         	Member memberInfo = (Member) loginResult.get("memberInfo");
-        	
+        	String status = memberInfo.getStatusCode();
         	String grade = memberInfo.getGradeCode();
         	log.info("로그인 등급: {}", grade);
         	
@@ -57,30 +57,37 @@ public class UserLoginController {
             session.setAttribute("SName", memberInfo.getMemberName());
             session.setAttribute("SGrd", memberInfo.getGradeCode());
             session.setAttribute("SCD", memberInfo.getMemberCode());
-            //날짜 업데이트
-            userLoginService.updateLoginDate(memberInfo);    
-            log.info("로그인 날짜 업데이트 호출: {}", member.getMemberId());
-            
-            if("USER".equals(grade)) {
-            	redirectAttributes.addFlashAttribute("success", "로그인에 성공하였습니다");
-            	log.info("로그인성공");    	
-                model.addAttribute("msg", "로그인에 성공하였습니다.");
-                model.addAttribute("url", "/");
-            	return "user/mypage/alert";
-            	
-            	
-        	}else if("ENTER".equals(grade)) {
-            	redirectAttributes.addFlashAttribute("success", "로그인에 성공하였습니다");
-            	log.info("로그인성공");
-                model.addAttribute("msg", "기업회원 로그인에 성공하였습니다.");
-                model.addAttribute("url", "/enter");
-            	return "user/mypage/alert";
-        	}else {
-        		model.addAttribute("msg", "올바르지못한 로그인 경로입니다");
+            if("SD_ACTIVE".equals(status)) {
+                //날짜 업데이트
+                userLoginService.updateLoginDate(memberInfo);    
+                log.info("로그인 날짜 업데이트 호출: {}", member.getMemberId());
+	            if("USER".equals(grade)) {
+	            	
+	            	redirectAttributes.addFlashAttribute("success", "로그인에 성공하였습니다");
+	            	log.info("로그인성공");    	
+	                model.addAttribute("msg", "로그인에 성공하였습니다.");
+	                model.addAttribute("url", "/");
+	            	return "user/mypage/alert";
+	        	}else if("ENTER".equals(grade)) {
+	            	redirectAttributes.addFlashAttribute("success", "로그인에 성공하였습니다");
+	            	log.info("로그인성공");
+	                model.addAttribute("msg", "기업회원 로그인에 성공하였습니다.");
+	                model.addAttribute("url", "/enter");
+	            	return "user/mypage/alert";
+	        	}else {
+	        		model.addAttribute("msg", "올바르지못한 로그인 경로입니다");
+	        		model.addAttribute("url", "/login");
+	        		return "user/mypage/alert";
+	        	}
+            }else if("SD_DORMANT".equals(status)) {
+            	model.addAttribute("msg", "휴면을 해제 후 로그인해주세요");
         		model.addAttribute("url", "/login");
         		return "user/mypage/alert";
-        	}
-
+            }else {
+            	model.addAttribute("msg", "로그인하실 수 없습니다.\n문의사항이 있을 경우 페이지 하단의 고객센터 연락처를 이용해주세요.");
+        		model.addAttribute("url", "/login");
+        		return "user/mypage/alert";
+            }
         } else {
         	model.addAttribute("error", "아이디 또는 비밀번호가 올바르지 않습니다.");
         	redirectAttributes.addFlashAttribute("error", "아이디 또는 비밀번호가 올바르지 않습니다.");
