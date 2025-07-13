1722440656adde8ca4ef197ec9072b523914c977
sepal <sepal@DESKTOP-HURAQ4I>
2025-07-13 20:30:52 +0900

    [outpolic-SSY] 회원뷰 생성 [상세 설명] 관리자 페이지의 회원뷰 구성. 전체/활성/휴면/탈퇴회원뷰를 만들고 제재회원은 따로 볼수있게 하려고 함.

--- outpolic/src/main/java/outpolic/admin/member/service/AdminMemberService.java
+++ outpolic/src/main/java/outpolic/admin/member/service/AdminMemberService.java
@@ -7,4 +7,10 @@ import outpolic.common.domain.Member;
 public interface AdminMemberService {
 	//회원 전체 목록 조회
 	List<Member> getMemberList();
+	
+	//활성 회원 목록 조회
+	List<Member> getActiveMemberList();
+	
+	//휴면 회원 목록 조회
+	List<Member> getWithdrawMemberList();
 }
