ad8ac95edb2b382e36a025454b9b4f2b3dc57318
User <User@DESKTOP-UONQLD0>
2025-07-13 15:39:35 +0900

    [outpolic-YSR] 중간커밋 detail : 외주 진행 상세 페이지

--- outpolic/src/main/java/outpolic/enter/osst/domain/EnterOsst.java
+++ outpolic/src/main/java/outpolic/enter/osst/domain/EnterOsst.java
@@ -4,41 +4,32 @@ import lombok.Data;
 
 @Data
 public class EnterOsst {
-	private String ospCode;
-	private String ospOcdCode; 
-	private String ospOcdTitle;
-	private String ospMemberCode;
-	private String ospMemberName;
-	private String ospEntCode;
-	private String ospEntMemberCode;
-	private String ospEntMemberName;
-	private String ospEntName; 
-	private String ospStcCode;  
-	private String ospStcName; 
-	private String ospSplyYmdt;  
+	private String ospCode;   
+	private String ocdCode;  
+	             
+	private String ocdTitle;  
+	private String ocdExpln;
+	private String ocdDmndYmdt;
+	private String ocdDedline;
+	private Integer ocdAmt;    
+	             
 	private Integer ospCustYn; 
+	private String ospSplyYmdt;
 	private String ospCustYmdt;
 	private String ospCustStcYn;
 	private Integer ospStcOrder;
-	
-	
-	
-	private String ocdCode;   
-	private String ocdMemberCode;
-	private String ocdTitle;    
-	private String ocdExpln; 
-	private String ocdDmndYmdt;
-	
-	private String osstDetailCode;
-	private String osstDetailMemberCode;
-	private String osstDetailMemberName;
-	private String osstDetailEntCode;
-	private String osstDetailEntName;  
-	private String osstDetailEntMbrName;
-	private String osstDetailTitle;
-	private String osstDetailExpln;  
-	private String osstDetailDedline;  
-	private String osstDetailAmt;  
-	private String osstDetailStcCode;
-	private String osstDetailDmndYmdt;
+	             
+	private String memberCode;
+	private String memberName;
+	private String memberEmail;
+	             
+	private String entCode;
+	private String entName;
+	private String entMbrName;
+	private String entMbrEmail;
+	private String entMemberCode;
+	private String entMemberName;
+	             
+	private String stcCode;
+	private String stcName;
 }
--- outpolic/src/main/java/outpolic/enter/osst/domain/EnterOsstRecord.java
+++ outpolic/src/main/java/outpolic/enter/osst/domain/EnterOsstRecord.java
@@ -4,14 +4,15 @@ import lombok.Data;
 
 @Data
 public class EnterOsstRecord {
-	private String osstDetailCode;
-	private String osstDetailMemberCode;
-	private String osstDetailMemberName;
-	private String osstDetailEntCode;
-	private String osstDetailEntName;
-	private String osstDetailEntMbrName;
-	private String osstDetailTitle;
-	private String osstDetailExpln;
-	private String osstDetailStcCode;
-	private String osstDetailDmndYmdt;
+	private String osstRecordCode;
+	private String osstPrograssCode;
+	private String osstRecordStcCode;
+	private String osstRecordStcName;
+	private String osstRecordType;
+	private String osstRecordMbrCode;
+	private String osstRecordMbrName;
+	private String osstRecordTitle;
+	private String osstRecordCn;
+	private String osstRecordRegYmdt;
+	private String osstRecordUpCode;
 }
