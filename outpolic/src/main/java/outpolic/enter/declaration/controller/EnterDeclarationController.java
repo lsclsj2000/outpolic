4b48f36774b999916ab6db49c7bc0883e5136713
tkfk1130 <salang291@gmail.com>
2025-07-15 19:15:45 +0900

    [outpolic-YSR] 네비게이션 및 controller 수정 detail : 네비게이션 텍스트 굵기 수정 및 controller 수정

--- outpolic/src/main/java/outpolic/enter/declaration/controller/EnterDeclarationController.java
+++ outpolic/src/main/java/outpolic/enter/declaration/controller/EnterDeclarationController.java
@@ -1,8 +1,10 @@
 package outpolic.enter.declaration.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -12,24 +14,27 @@ public class EnterDeclarationController {
 	
 	
 	@GetMapping("/enterDeclarationWrite")
-	public String enterDeclarationWriteView() {
+	public String enterDeclarationWriteView(Model model) {
 		// 신고 작성 페이지
+		model.addAttribute("title", "신고 작성");
 		
 		return "enter/declaration/enterDeclarationWriteView";
 	}
 	
 	
 	@GetMapping("/enterDeclarationNotice")
-	public String enterDeclarationNoticeView() {
+	public String enterDeclarationNoticeView(Model model) {
 		// 신고 주의사항 페이지
+		model.addAttribute("title", "신고 주의사항");
 		
 		return "enter/declaration/enterDeclarationNoticeView";
 	}
 
 	
 	@GetMapping("/enterDeclaration")
-	public String enterDeclarationView() {
+	public String enterDeclarationView(Model model) {
 		// 나의 신고 내역 목록 페이지
+		model.addAttribute("title", "신고 내역");
 		
 		return "enter/declaration/enterDeclarationView";
 	}
