734aadc6a98c6b07bc45311401e00ff87b591732
uzuMakiOneStar <wlfkfss2323@gmail.com>
2025-07-15 11:45:28 +0900

    [outpolic-KHB] 오류발생해서 다시 커밋해용 detail : 서비스임플에 서비스 어노테이션 안넣어서 오류생김

--- outpolic/src/main/java/outpolic/admin/statistics/service/impl/AdminRankingServiceImpl.java
+++ outpolic/src/main/java/outpolic/admin/statistics/service/impl/AdminRankingServiceImpl.java
@@ -3,12 +3,14 @@ package outpolic.admin.statistics.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import outpolic.admin.statistics.domain.AdminPortfolioRankingContentsDTO;
 import outpolic.admin.statistics.domain.AdminRankingContentsDTO;
 import outpolic.admin.statistics.mapper.AdminRankingMapper;
 import outpolic.admin.statistics.service.AdminRankingService;
 
+@Service
 public class AdminRankingServiceImpl implements AdminRankingService{
 
 	@Autowired
