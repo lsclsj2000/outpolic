0987d16acf912fd71694d9f0f13415fa8c18828f
shindidit <bionell430@gmail.com>
2025-07-16 15:20:06 +0900

    [outpolic-SSY] 관리자 로그인 수정 detail : 에러 그만떠

--- outpolic/src/main/java/outpolic/admin/login/service/Impl/AdminLoginServiceImpl.java
+++ outpolic/src/main/java/outpolic/admin/login/service/Impl/AdminLoginServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import outpolic.admin.login.DTO.AdminLoginDTO;
 import outpolic.admin.login.mapper.AdminLoginMapper;
 import outpolic.admin.login.service.AdminLoginService;
 import outpolic.common.domain.Member;
@@ -20,17 +21,29 @@ public class AdminLoginServiceImpl implements AdminLoginService {
 
 	@Override
 	public Map<String, Object> loginAdmin(String memberId, String memberPw) {
+		System.out.println("[DEBUG] loginAdmin 메서드 진입");
 		Map<String, Object> resultMap = new HashMap<>();
 		boolean isMatched = false;
-		Member member = adminLoginMapper.findAdminMemberById(memberId);
-		if(member != null && passwordEncoder.matches(memberPw, member.getMemberPw())) {
-			isMatched = true;
-			resultMap.put("memberInfo", member);
+		
+		AdminLoginDTO adminLoginDTO = adminLoginMapper.findAdminMemberById(memberId);
+		
+		if(adminLoginDTO != null && adminLoginDTO.getMemberInfo() != null) {
+			Member member = adminLoginDTO.getMemberInfo();
+			
+			System.out.println("입력된 비밀번호(평문): " + memberPw);
+			System.out.println("DB 저장된 비밀번호(암호): " + member.getMemberPw());
+			System.out.println("매치 결과: " + passwordEncoder.matches(memberPw, member.getMemberPw()));
+
+			if(member != null && passwordEncoder.matches(memberPw, member.getMemberPw())) {
+				
+				isMatched = true;
+				resultMap.put("adminLoginDTO", adminLoginDTO); 
+			}
 		}
 		resultMap.put("isMatched", isMatched);
 		return resultMap;
-	}
 	
+	}
 	//member테이블에 마지막 로그인 일시 update
 	@Override
 	public void updateAdminMemberLoginDate(Member member) {
