2c7877ff49ac3e151f063dce873be44b38ca3701
uzuMakiOneStar <wlfkfss2323@gmail.com>
2025-07-16 21:58:44 +0900

    [outpolic-KHB] 회사 정보 페이지 많듬 detail : 리뷰 삭제점

---
+++ outpolic/src/main/java/outpolic/user/company/domain/UserCompanyOutsourcingDTO.java
@@ -0,0 +1,11 @@
+package outpolic.user.company.domain;
+
+import lombok.Data;
+
+@Data
+public class UserCompanyOutsourcingDTO {
+
+	private String osCd;
+	private String osTtl;
+	private int osAmt;
+}
