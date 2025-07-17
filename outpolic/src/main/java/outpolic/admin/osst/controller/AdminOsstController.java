9844179a268c63ed0dca22761ec63a304c4461d1
tkfk1130 <salang291@gmail.com>
2025-07-17 19:06:06 +0900

    [outpolic-YSR] 관리자 진행 외주 수정 detail : 기능 수정

--- outpolic/src/main/java/outpolic/admin/osst/controller/AdminOsstController.java
+++ outpolic/src/main/java/outpolic/admin/osst/controller/AdminOsstController.java
@@ -1,18 +1,32 @@
 package outpolic.admin.osst.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import outpolic.admin.osst.domain.AdminOsst;
+import outpolic.admin.osst.service.AdminOsstService;
+
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminOsstController {
 	
+	private final AdminOsstService adminOsstService; 
+	
 	@GetMapping("/adminOsst")
 	public String adminOutsourcingStatusView(Model model) {
+		// 외주 진행 목록 조회
+		List<AdminOsst> adminOsst = adminOsstService.getAdminOsstList();
 		
 		model.addAttribute("title", "외주 진행 현황");
+		model.addAttribute("adminOsst", adminOsst);
 		
 		return "admin/osst/adminOutsourcingStatusView";
 	}
