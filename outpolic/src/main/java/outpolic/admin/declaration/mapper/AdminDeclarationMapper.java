06d8e283a4806acaaf1e9cf9b4a9cef622b65639
tkfk1130 <salang291@gmail.com>
2025-07-18 17:05:22 +0900

    [outpolic-YSR] 관리자 신고 자원 등록 detail : 신고 타입, 사유, 처리결과코드 등록 완료. 이제 수정만 하면 돼요,,,

--- outpolic/src/main/java/outpolic/admin/declaration/mapper/AdminDeclarationMapper.java
+++ outpolic/src/main/java/outpolic/admin/declaration/mapper/AdminDeclarationMapper.java
@@ -9,6 +9,21 @@ import outpolic.admin.declaration.domain.AdminDeclaration;
 @Mapper
 public interface AdminDeclarationMapper {
 	
+	// 신고 타입 등록
+	void insertDeclarationType(AdminDeclaration declaration);
+	
+	// 신고 사유 등록
+    void insertDeclarationReason(AdminDeclaration declaration);
+    
+    // 신고처리결과 등록
+    void insertDeclarationResult(AdminDeclaration declaration);
+	
+	// 신고 처리 결과 자원 조회
+	List<AdminDeclaration> getAdminDeclarationResultList();
+	
+	// 신고 사유 자원 조회
+	List<AdminDeclaration> getAdminDeclarationReasonList();
+	
 	// 신고 타입 자원 조회
 	List<AdminDeclaration> getAdminDeclarationTypeList();
 	
