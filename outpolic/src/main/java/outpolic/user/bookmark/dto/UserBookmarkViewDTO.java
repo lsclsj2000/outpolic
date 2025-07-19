f7913d2befd24693d4284d11104f907ae7ce2e03
shindidit <bionell430@gmail.com>
2025-07-19 18:53:57 +0900

    [outpolic-SSY] 즐겨찾기 뷰 제작 detail : 즐겨찾기 뷰 만들고 한별이꺼 레이아웃도 수정함. 쿼리까지 짜둬서 집가서 데이터 매핑 할 수도있고 아닐수도있고

---
+++ outpolic/src/main/java/outpolic/user/bookmark/dto/UserBookmarkViewDTO.java
@@ -0,0 +1,35 @@
+package outpolic.user.bookmark.dto;
+
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class UserBookmarkViewDTO {
+	
+	 // 북마크 정보
+    private String bmCd;
+    private String clCd;
+    private String clType;         // ENTER / PRTF / OS 등
+    private LocalDateTime bmRegYmdt;
+
+    // 공통 콘텐츠 정보
+    private String title;          // 기업명 or 포트폴리오 제목 or 외주 제목
+    private String writer;         // 기업명
+    private String ctgryNm;        // 카테고리 이름
+    private String thumbnailUrl;   // 포폴/외주라면 썸네일
+
+    // 외주일 경우
+    private String osCd;
+    private String osAmt;
+
+    // 포트폴리오일 경우
+    private String prtfCd;
+    private int prtfDwnldCnt;
+
+    // 기업일 경우
+    private String entCd;
+    private String entRprsv;
+    private Double entScr;
+}
---
+++ outpolic/src/main/java/outpolic/user/bookmark/mapper/UserBookmarkMapper.java
@@ -0,0 +1,8 @@
+package outpolic.user.bookmark.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserBookmarkMapper {
+
+}
