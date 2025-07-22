e4e886f64997f5d38a2798e5a99d0ef07de2005f
shindidit <bionell430@gmail.com>
2025-07-22 12:06:21 +0900

    [outpolic-SSY] admin 기업회원쪽 정리중 detail : listView 작성중.

---
+++ outpolic/src/main/java/outpolic/admin/member/service/Impl/adminEnterServiceImpl.java
@@ -0,0 +1,73 @@
+package outpolic.admin.member.service.Impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import outpolic.admin.member.dto.AdminMemberDTO;
+import outpolic.admin.member.mapper.AdminEnterMapper;
+import outpolic.admin.member.service.AdminEnterService;
+import outpolic.common.domain.Member;
+@Service
+@RequiredArgsConstructor
+public class adminEnterServiceImpl implements AdminEnterService {
+	
+	private final AdminEnterMapper adminEnterMapper;
+	
+	@Override
+	public List<AdminMemberDTO> getEnterList() {
+		List<AdminMemberDTO> enterList = adminEnterMapper.getEnterList();			
+		return enterList;
+	}
+
+
+	@Override
+	public List<AdminMemberDTO> getEnterListByStatus(String statusCode) {
+		String status;
+		switch(statusCode.toLowerCase()) {
+			case "active":
+	            status = "SD_ACTIVE";
+	            break;
+	        case "withdrawn":
+	            status = "SD_WITHDRAWN";
+	            break;
+	        case "dormant":
+	            status = "SD_DORMANT";
+	            break;
+	        case "limit":
+	            status = "SD_LIMIT";
+	            break;
+	        default:
+	            status = null; 
+		}
+		List<AdminMemberDTO> enterList = adminEnterMapper.getEnterListByStatus(status);
+		return enterList;
+	}
+	
+
+	@Override
+	public AdminMemberDTO selectEnterByCode(String memberCode) {
+		return adminEnterMapper.selectEnterByCode(memberCode);
+	}
+
+	@Override
+	public int updateAdminEnterEditInfo(AdminMemberDTO adminMemberDTO) {
+		int result = adminEnterMapper.updateAdminEnterEditInfo(adminMemberDTO);
+		return result;
+	}
+
+	@Override
+	public List<AdminMemberDTO> selectFilteredEnterpriseMembers(String statusCode) {
+		return adminEnterMapper.selectFilteredEnterpriseMembers(statusCode);
+	}
+
+
+	@Override
+	public List<AdminMemberDTO> searchEnterpriseMembers(String keyword) {
+		return adminEnterMapper.searchEnterpriseMembers(keyword);
+	}
+
+
+
+}
